91075cd43e694d4d9c9f5f9e3cd548bb1c5c6230
mickaeldamathadev <127761598+mickaeldamathadev@users.noreply.github.com>
2023-11-08 15:50:57 +0100

    v $version

--- src/ROLES/views/Roles.tsx
+++ src/ROLES/views/Roles.tsx
@@ -1,15 +1,16 @@
-import { Link, useLoaderData } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import DocumentTable from 'ultimate-react-mongo-table'
 
-export default function Roles() {
-  const { roles }: any = useLoaderData()
-
+export default function Roles(props: { roles: any[] }) {
   return (
     <div className="roles flex-start">
       <Link className="add-role" to="/app/admin/roles/new">
         +
       </Link>
-      <DocumentTable<any> documents={roles.data as any[]} action={() => null} />
+      <DocumentTable<any>
+        documents={props.roles as any[]}
+        action={() => null}
+      />
     </div>
   )
 }
