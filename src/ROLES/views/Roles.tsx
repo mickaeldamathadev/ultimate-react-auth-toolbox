be4bfc1c6027b0500e9d4b4a94c7076b78bca710
mickaeldamathadev <127761598+mickaeldamathadev@users.noreply.github.com>
2023-11-08 17:01:34 +0100

    v $version

--- src/ROLES/views/Roles.tsx
+++ src/ROLES/views/Roles.tsx
@@ -3,12 +3,10 @@ import DocumentTable from 'ultimate-react-mongo-table'
 
 export default function Roles() {
   const roles = useLoaderData()
+
   return (
     <div className="roles flex-start">
-      <DocumentTable<any>
-        documents={(roles as any[]) || []}
-        action={() => null}
-      />
+      <DocumentTable<any> documents={roles as any[]} action={() => null} />
     </div>
   )
 }
