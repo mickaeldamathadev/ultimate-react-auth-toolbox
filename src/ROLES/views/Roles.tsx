9884a4d1b002dbcc0eb38385ada7b59e7062c1aa
mickaeldamathadev <127761598+mickaeldamathadev@users.noreply.github.com>
2023-11-08 15:48:53 +0100

    v $version

--- src/ROLES/views/Roles.tsx
+++ src/ROLES/views/Roles.tsx
@@ -1,10 +1,8 @@
-import { Link, useLoaderData, useNavigate } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
 import DocumentTable from 'ultimate-react-mongo-table'
 
 export default function Roles() {
   const { roles }: any = useLoaderData()
-  const nav = useNavigate()
-  console.log(roles.data)
 
   return (
     <div className="roles flex-start">
