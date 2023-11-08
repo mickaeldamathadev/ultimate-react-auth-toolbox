a59ce7c36972ccaf5126432c98be7c7eecd2481f
mickaeldamathadev <127761598+mickaeldamathadev@users.noreply.github.com>
2023-11-08 16:05:55 +0100

    v $version

--- src/PERMISSIONS/views/Permissions.tsx
+++ src/PERMISSIONS/views/Permissions.tsx
@@ -1,4 +1,4 @@
-import { Link, useLoaderData } from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom'
 import DocumentTable from 'ultimate-react-mongo-table'
 import { IPermission } from '../data/types'
 
@@ -6,8 +6,6 @@ export default function Permissions() {
   const permissions: any = useLoaderData()
   return (
     <div className="flex-start">
-      <Link to="/app/admin/permissions/new">Creer une NOUVEL Permission</Link>
-
       <DocumentTable<IPermission> documents={permissions.data as any[]} />
     </div>
   )
--- src/ROLES/views/Roles.tsx
+++ src/ROLES/views/Roles.tsx
@@ -1,12 +1,8 @@
-import { Link } from 'react-router-dom'
 import DocumentTable from 'ultimate-react-mongo-table'
 
 export default function Roles(props: { roles: any[] }) {
   return (
     <div className="roles flex-start">
-      <Link className="add-role" to="/app/admin/roles/new">
-        +
-      </Link>
       <DocumentTable<any>
         documents={props.roles as any[]}
         action={() => null}
