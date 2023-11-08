a58fd3b9f51bb1db04fe7ebbbfc98e859a181f4c
mickaeldamathadev <127761598+mickaeldamathadev@users.noreply.github.com>
2023-11-08 16:57:53 +0100

    v $version

--- src/ROLES/views/Roles.tsx
+++ src/ROLES/views/Roles.tsx
@@ -5,7 +5,10 @@ export default function Roles() {
   const roles = useLoaderData()
   return (
     <div className="roles flex-start">
-      <DocumentTable<any> documents={roles as any[]} action={() => null} />
+      <DocumentTable<any>
+        documents={(roles as any[]) || []}
+        action={() => null}
+      />
     </div>
   )
 }
