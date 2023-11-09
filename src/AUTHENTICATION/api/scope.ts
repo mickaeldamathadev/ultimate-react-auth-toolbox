42c4309de08dd9d56fe83a255cba58103f74a301
mickaeldamathadev <127761598+mickaeldamathadev@users.noreply.github.com>
2023-11-09 09:51:30 +0100

    v $version

--- src/AUTHENTICATION/api/scope.ts
+++ src/AUTHENTICATION/api/scope.ts
@@ -4,8 +4,6 @@ export default async function verifyScope(scope: string) {
   try {
     const request = await get('/verifyScope/' + scope)
 
-    console.log(request)
-
     if (request.error) {
       throw new Error(request.error)
     }
